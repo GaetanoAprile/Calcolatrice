ca3ff702af187366d62f8040baa6875cd1cdaf85
GaetanoAprile <g.aprile5@studenti.unisa.it>
2025-10-06 10:19:15 +0200

    Multi + test

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -8,5 +8,9 @@ public class Calculator {
         return a-b;
     }
 
+    public int multi(int a, int b){
+        return a*b;
+    }
+
 
 }
--- src/test/java/CalculatortTester.java
+++ src/test/java/CalculatortTester.java
@@ -1,5 +1,8 @@
 public class CalculatortTester {
     public static void main(String[] args) {
-
+int a= 5;
+int b= 5;
+Calculator c = new Calculator();
+System.out.println(c.multi(a,b));
     }
 }
